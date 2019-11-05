81e6e348c475a754cfed2dea23ba0d430a236fb3
zempo <zelenkosolomon@gmail.com>
2019-11-05 13:30:32 -0800

    add script metadata

--- src/Components/Routes/ScriptEditor.js
+++ src/Components/Routes/ScriptEditor.js
@@ -1,14 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import dateFormat from "dateformat";
 import { AtlSection } from "../Utils/Utils";
 
-const ScriptEditor = () => {
+const ScriptEditor = (props) => {
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [date, setDate] = useState("");
+  const [actors, setActors] = useState([]);
+  const [tags, setTags] = useState([]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    if (props.location.state !== undefined) {
+      const { item } = props.location.state;
+      console.log(item.id);
+      setTitle(item.title);
+      setSubtitle(item.subtitle);
+      setAuthor(item.author);
+      setActors(item.actors);
+      setTags(item.tags);
+      let date;
+      date = dateFormat(item.date_created, "mmmm d, yyyy");
+      if (item.date_updated !== null) {
+        date = dateFormat(item.date_updated, "mmmm d, yyyy");
+      }
+      setDate(date);
+    }
   }, []);
 
   return (
     <AtlSection>
       <h1 className="animated-h1">Editor</h1>
+      {title}
+      <br />
+      {subtitle}
+      <br />
+      {author}
+      <br />
+      {date}
     </AtlSection>
   );
 };
