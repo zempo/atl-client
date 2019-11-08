f16fb48f52e78bf70ccea73a8718692a5d3f4ae0
zempo <zelenkosolomon@gmail.com>
2019-11-08 11:56:37 -0800

    can add tags

--- src/Components/Utils/Editor/Inputs.js
+++ src/Components/Utils/Editor/Inputs.js
@@ -5,13 +5,15 @@ import "../Styles/Editor.css";
 import { EditContext } from "../../../Contexts/EditContext";
 import { readScripts } from "../../../Services/endpoints-service";
 
-const Input = ({ currentId, body, actors, tags }) => {
+const Input = ({ currentId, body }) => {
   const {
     value: { tenthHeight, tenthWidth }
   } = useContext(StyleContext);
   const {
     value: { currentScript, updateScriptBody }
   } = useContext(EditContext);
+  const [actors, setActors] = useState([]);
+  const [tags, setTags] = useState([]);
   const [currentBody, setCurrentBody] = useState(body);
 
   useEffect(() => {
@@ -19,6 +21,8 @@ const Input = ({ currentId, body, actors, tags }) => {
       try {
         const result = await readScripts.get(`/${currentId}`);
         setCurrentBody(result.data[0].body);
+        setActors(result.data[0].actors);
+        setTags(result.data[0].tags);
       } catch (error) {
         console.log(error);
       }
@@ -28,16 +32,48 @@ const Input = ({ currentId, body, actors, tags }) => {
   }, []);
 
   const handleChange = (e) => {
-    // let current = dialogue
-    // setDialogue(e.target.value)
     setCurrentBody(e.target.value);
     updateScriptBody(currentScript, e.target.value);
+  };
+
+  const appendTag = (e) => {
+    e.preventDefault();
+    let currentTag = ` [${e.target.innerHTML}]`;
+    setCurrentBody(currentBody + currentTag);
   };
 
   return (
     <>
       <Box className="box box-top" height={tenthHeight * 5} width={tenthWidth * 7.92} axis="both" resizeHandles={["s"]}>
         <form className="input-tags">
+          <fieldset className="actors">
+            {actors.map((actor, i) => {
+              return (
+                <div key={i} className="actor-control">
+                  <button className="append-tag" id={i} onClick={appendTag}>
+                    {actor}
+                  </button>
+                  <button className="delete-tag" id={i}>
+                    x
+                  </button>
+                </div>
+              );
+            })}
+          </fieldset>
+          <fieldset className="tags">
+            {tags.map((tag, i) => {
+              return (
+                <div key={i} className="tag-control">
+                  <button className="append-tag" id={i} onClick={appendTag}>
+                    {tag}
+                  </button>
+                  <button className="delete-tag" id={i}>
+                    x
+                  </button>
+                </div>
+              );
+            })}
+          </fieldset>
           <textarea value={currentBody} onChange={handleChange} />
         </form>
       </Box>
