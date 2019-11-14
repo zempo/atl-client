62d3bcf63d260a191a57c24d94df088759e6561f
zempo <zelenkosolomon@gmail.com>
2019-11-14 12:56:38 -0800

    add copy relay

--- src/Components/Forms/ModalForms/CopyScript.js
+++ src/Components/Forms/ModalForms/CopyScript.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { AtlNotification } from "../../Utils/Utils";
+import { readScripts } from "../../../Services/endpoints-service";
 
 const CopyScript = ({ item, cancel }) => {
-  const handleCopy = e => {
+  const [resMsg, setResMsg] = useState("");
+  const [resStatus, setResStatus] = useState(0);
+
+  const handleCopy = async e => {
     e.preventDefault();
+    setResStatus(0);
+    setResMsg("");
+
+    try {
+      const scriptToCopy = await readScripts.get(`/${item}`);
+
+      console.log(scriptToCopy.data[0]);
+      setResStatus(200);
+      setResMsg("Copied Script");
+    } catch (error) {
+      setResStatus(error.response.status);
+      setResMsg(Object.values(error.response.data.error));
+      setTimeout(() => {
+        setResStatus(0);
+      }, 5000);
+    }
   };
   return (
     <div className="modal-action copy-script">
-      {item}
+      {resStatus === 0 ? null : (
+        <AtlNotification type={resStatus} msg={resMsg} />
+      )}
       <button className="modal-btn" onClick={cancel}>
         Cancel
       </button>
