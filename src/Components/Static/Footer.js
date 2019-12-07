b1ffddd6e9fd895d54d94eb9b31124defef24a43
zempo <zelenkosolomon@gmail.com>
2019-12-06 17:35:04 -0800

    add signature

--- src/Components/Static/Footer.js
+++ src/Components/Static/Footer.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "./Styles/Static.css";
 import { UserContext } from "../../Contexts/UserContext";
+import Sig from "../../Images/sig.svg";
 
 const Footer = () => {
   // add a scroll to top button, additional map menu, eventual site map, and custom signature
@@ -10,7 +11,17 @@ const Footer = () => {
   return (
     <footer className="atl-footer" style={{ background: `${userColor}` }}>
       <h2>Above The Line</h2>
-      <h3>Created by Solomon Zelenko</h3>
+      <h3>
+        Created by{" "}
+        <a
+          className="signature"
+          href="https://github.com/zempo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Sig} alt="Solomon Zelenko" />
+        </a>
+      </h3>
     </footer>
   );
 };
